f40e184fa4e3bde055eab1a876332cbaa7f74c8b
李智勇 <george510257@gmail.com>
2024-04-02 18:03:15 +0800

    add ollama

--- gemini-ai-boot/src/main/java/com/gls/gemini/ai/boot/web/controller/AiController.java
+++ gemini-ai-boot/src/main/java/com/gls/gemini/ai/boot/web/controller/AiController.java
@@ -2,15 +2,21 @@ package com.gls.gemini.ai.boot.web.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ai")
 public class AiController {
     @Resource
     private ChatClient chatClient;
+    @Resource
+    private EmbeddingClient embeddingClient;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,5 +26,10 @@ public class AiController {
     @GetMapping("/ask")
     public String ask(String question) {
         return chatClient.call(question);
+    }
+
+    @GetMapping("/answer")
+    public Map<String, Object> answer(String question) {
+        return Map.of("answer", embeddingClient.embedForResponse(List.of(question)));
     }
 }
